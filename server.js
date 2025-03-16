8caea64f255484587d7d402651278f05444b827a
khmiq <mumtozabonukhamidullayeva7@gmail.com>
2025-03-16 12:38:27 +0500

    added

--- server.js
+++ server.js
@@ -93,3 +93,5 @@ app.delete("/words/:id", async (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app
